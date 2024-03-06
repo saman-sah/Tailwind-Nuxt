686f65205e5d2275421ec928a795889e223cbec0
SamanSah <sahraei.saman91@gmail.com>
2024-03-07 02:42:26 +0330

    complete left section of hero

--- components/hero.vue
+++ components/hero.vue
@@ -4,21 +4,28 @@
     class="w-full flex xl:flex-row flex-col justify-start min-h-screen gap-10 max-container"
   >
     <div class="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl: padding-x pt-28">
-      <p>Our Summer Collection</p>
-      <h1 class="text-red-400">
-        <span>The New Arrival</span>
+      <p class="text-xl font-montserrat text-coral-red">Our Summer Collection</p>
+      <h1 class="text-8xl  mt-10 font-palanquin max-sm:text-[72px] max-sm:leading-[82px] font-bold">
+        <span class="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">The New Arrival</span>
         <br />
-        <span>Nike</span>
+        <span class="text-coral-red inline-block mt-3">Nike</span>
         Shoes
       </h1>
-      <p>
+      <p class="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
         Discover Stylish Nike arrivals, quality comfort, and innovation for your active life.
       </p>
       <Button label="Click me!" :iconUrl="arrowRight" />
+      <div class="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
+        <div v-for="(stat, index) in statistics" :key="index">
+          <p class="text-4xl font-palanquin font-bold">{{ stat.value }}</p>
+          <p class="leading-7 font-montserrat text-slate-gray" >{{ stat.label }}</p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
 
 <script setup>
-import { arrowRight } from 'assets/icons/index'
+import { arrowRight } from '~/assets/icons'
+import {statistics } from '~/utils/constant.js'
 </script>
--- utils/constant.js
+++ utils/constant.js
@@ -16,3 +16,19 @@ export const NAV_LINKS = [
     label: 'Contact Us'
   },
 ]
+
+export const statistics = [
+  {
+    value: '1K+',
+    label: 'Brands'
+  },
+  {
+    value: '500K+',
+    label: 'Shops'
+  },
+  {
+    value: '250K+',
+    label: 'Customers'
+  },
+  
+]
