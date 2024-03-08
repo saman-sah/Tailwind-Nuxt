fcc956d62a34141efd579675661c47b7394401a4
SamanSah <sahraei.saman91@gmail.com>
2024-03-09 00:35:34 +0330

    init customer review

--- components/customerReviews.vue
+++ components/customerReviews.vue
@@ -1,5 +1,21 @@
 <template>
-  Customer Reviews
+  <section class="max-container">
+    <h3 class="font-palanquin text-center text-4xl font-bold">
+      What Our
+      <span class="text-coral-red">
+        Customers
+      </span>
+      Says?      
+    </h3>
+    <p class="info-text m-auto mt-4 max-w-lg text-center">
+      Hear genuine stories from our satisfied customers about their
+      exceptional experiences with us.
+    </p>
+
+    <div class="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+
+    </div>
+  </section>
 </template>
 
 <script setup>
---
+++ components/reviews/item.vue
@@ -0,0 +1,6 @@
+<template>
+    fdsfdf
+</template>
+
+<script setup>
+</script>
