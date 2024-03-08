4f7158a4179f5dd34c0081781ebcfab9b7676122
SamanSah <sahraei.saman91@gmail.com>
2024-03-08 18:47:30 +0330

    update item file

--- components/popular-products/item.vue
+++ components/popular-products/item.vue
@@ -1,10 +1,23 @@
 <template>
     <div class="flex flex-1 flex-col w-full max-sm:w-full">
-        <img :src="imgUrl" alt="">
+        <img 
+          :src="imgUrl" 
+          :alt="name"
+          class="w-[280px] h-[280px]"
+        >
+        <div class="mt-8 flex justify-center gap-2.5">
+            <img 
+              :src="star" 
+              alt="rating"
+              width="24px"
+              height="24px"
+            />
+        </div>
     </div>
 </template>
 
 <script setup>
+import { star } from '~/assets/icons'
 defineProps({
     name: {
         type: String,
