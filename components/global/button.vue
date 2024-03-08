f8f628aa6b61464c999648bc4d81e73185558ce3
SamanSah <sahraei.saman91@gmail.com>
2024-03-08 20:41:50 +0330

    fix global button

--- components/global/button.vue
+++ components/global/button.vue
@@ -2,9 +2,10 @@
   <button class="flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none bg-coral-red rounded-full text-white border-coral-red">
     {{ label }}
     <img 
-    :src="iconUrl" 
-    class="ml-2 rounded-full w-5 h-5"
-    alt=""
+      v-if="iconUrl"
+      :src="iconUrl" 
+      class="ml-2 rounded-full w-5 h-5"
+      alt=""
     />
   </button>
 </template>
