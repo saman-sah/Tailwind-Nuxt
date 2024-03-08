e8ce43558ec1282e590bf4fb6d1c7ce0bc2adbc1
SamanSah <sahraei.saman91@gmail.com>
2024-03-08 21:32:40 +0330

    complete services component

--- components/services.vue
+++ components/services.vue
@@ -1,7 +1,11 @@
 <template>
   <section class="max-container flex justify-center flex-wrap gap-9">
     <div v-for="(service, index) in services" :key="index">
-      {{ service.label }}
+      <ServicesItem 
+        :imgURL="service.imgURL"
+        :label="service.label"
+        :subtext="service.subtext"
+      />
     </div>
   </section>
 </template>
