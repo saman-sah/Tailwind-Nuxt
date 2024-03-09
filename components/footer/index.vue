2d9da427c62f238460bb5dc3c83b608edb411f33
saman-sah <sahraei.saman91@gmail.com>
2024-03-09 20:34:21 +0330

    init footer

--- components/footer/index.vue
+++ components/footer/index.vue
@@ -1,6 +1,24 @@
 <template>
-  Footer
+  <footer class="max-container">
+    <div class="flex justify-between items-center gap-20 flex-wrap max-lg:flex-col">
+      <div class="flex flex-col items-center">
+        <NuxtLink to="/">
+          <img 
+            :src="footerLogo" 
+            alt="footer logo"
+            width="150px"
+            height="46px"
+          />
+        </NuxtLink>
+        <p class="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
+          Get shoes ready for the new term at your nearest Nike store. Find
+          Your perfect Size In Store. Get Rewards
+        </p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <script setup>
+import { footerLogo } from '~/assets/images';
 </script>
